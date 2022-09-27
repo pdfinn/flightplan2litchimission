59957ef0dcb4b2454d7981574770361a978a4567
pdfinn <pdfinn@users.noreply.github.com>
2022-09-26 20:56:15 -0500

    Code clean-up.

--- fp2lm.go
+++ fp2lm.go
@@ -5,6 +5,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strconv"
 )
@@ -57,6 +58,61 @@ type LitchiWaypoint struct {
 	poi_altitudemode   int8
 	photo_timeinterval float32
 	photo_distinterval float32
+}
+
+func main() {
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		//defer fmt.Println("Usage: fp2lm [flightPlannerWaypoints.csv]\n")
+		log.Fatal(err)
+	}
+
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(f)
+
+	rows, err := readData(f)
+	if err != nil {
+		panic(err)
+	}
+
+	// create an instance of the waypoint with default values
+	waypoint := createNewWaypoint()
+
+	// print the Litchi Mission header
+	fmt.Println("latitude, longitude, altitude(m), heading(deg), curvesize(m), rotationdir, gimbalmode, " +
+		"gimbalpitchangle, actiontype1, actionparam1, actiontype2, actionparam2, actiontype3, actionparam3, " +
+		"actiontype4, actionparam4, actiontype5, actionparam5, actiontype6, actionparam6, actiontype7, " +
+		"actionparam7, actiontype8, actionparam8, actiontype9, actionparam9, actiontype10, actionparam10," +
+		" actiontype11, actionparam11, actiontype12, actionparam12, actiontype13, actionparam13, actiontype14," +
+		" actionparam14, actiontype15, actionparam15, altitudemode, speed(m/s), poi_latitude, poi_longitude, " +
+		"poi_altitude(m), poi_altitudemode, photo_timeinterval, photo_distinterval")
+
+	// print the individual records/waypoints
+	for _, row := range rows {
+		waypoint.longitude, err = strconv.ParseFloat(row[5], 64)
+		waypoint.latitude, err = strconv.ParseFloat(row[6], 64)
+		altitude, _ := strconv.ParseFloat(row[3], 32)
+		waypoint.altitude = float32(altitude)
+		fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v,%v,%v,%v,%v, %v, %v, %v, %v,%v, %v, "+
+			"%v, %v, %v,%v, %v, %v, %v, %v,%v, %v, %v, %v, %v,%v, %v, %v, %v, %v,%v, %v, %v, %v, %v, %v \n",
+			waypoint.latitude, waypoint.longitude, waypoint.altitude, waypoint.heading, waypoint.curvesize,
+			waypoint.rotationdir, waypoint.gimblemode, waypoint.gimbalpitchangle, waypoint.actiontype1,
+			waypoint.actionparam1, waypoint.actiontype2, waypoint.actionparam2, waypoint.actiontype3,
+			waypoint.actionparam3, waypoint.actiontype4, waypoint.actionparam4, waypoint.actiontype5,
+			waypoint.actionparam5, waypoint.actiontype6, waypoint.actionparam6, waypoint.actiontype7,
+			waypoint.actionparam7, waypoint.actiontype8, waypoint.actionparam8, waypoint.actiontype9,
+			waypoint.actionparam9, waypoint.actiontype10, waypoint.actionparam10, waypoint.actiontype11,
+			waypoint.actionparam11, waypoint.actiontype12, waypoint.actionparam12, waypoint.actiontype13,
+			waypoint.actionparam13, waypoint.actiontype14, waypoint.actionparam14, waypoint.actiontype15,
+			waypoint.actionparam15, waypoint.altitudemode, waypoint.speed, waypoint.poi_latitude,
+			waypoint.poi_longitude, waypoint.poi_altitude, waypoint.poi_altitudemode, waypoint.photo_timeinterval,
+			waypoint.photo_distinterval)
+	}
 }
 
 // handler function for creating new waypoints
@@ -130,59 +186,4 @@ func readData(rs io.ReadSeeker) ([][]string, error) {
 		return nil, err
 	}
 	return rows, nil
-}
-
-func main() {
-
-	f, err := os.Open(os.Args[1])
-	if err != nil {
-		//defer fmt.Println("Usage: fp2lm [flightPlannerWaypoints.csv]\n")
-		panic(err)
-	}
-
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(f)
-
-	rows, err := readData(f)
-	if err != nil {
-		panic(err)
-	}
-
-	// create an instance of the waypoint with default values
-	waypoint := createNewWaypoint()
-
-	// print the Litchi Mission header
-	fmt.Println("latitude, longitude, altitude(m), heading(deg), curvesize(m), rotationdir, gimbalmode, " +
-		"gimbalpitchangle, actiontype1, actionparam1, actiontype2, actionparam2, actiontype3, actionparam3, " +
-		"actiontype4, actionparam4, actiontype5, actionparam5, actiontype6, actionparam6, actiontype7, " +
-		"actionparam7, actiontype8, actionparam8, actiontype9, actionparam9, actiontype10, actionparam10," +
-		" actiontype11, actionparam11, actiontype12, actionparam12, actiontype13, actionparam13, actiontype14," +
-		" actionparam14, actiontype15, actionparam15, altitudemode, speed(m/s), poi_latitude, poi_longitude, " +
-		"poi_altitude(m), poi_altitudemode, photo_timeinterval, photo_distinterval")
-
-	// print the individual records/waypoints
-	for _, row := range rows {
-		waypoint.longitude, err = strconv.ParseFloat(row[5], 64)
-		waypoint.latitude, err = strconv.ParseFloat(row[6], 64)
-		altitude, _ := strconv.ParseFloat(row[3], 32)
-		waypoint.altitude = float32(altitude)
-		fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v,%v,%v,%v,%v, %v, %v, %v, %v,%v, %v, "+
-			"%v, %v, %v,%v, %v, %v, %v, %v,%v, %v, %v, %v, %v,%v, %v, %v, %v, %v,%v, %v, %v, %v, %v, %v \n",
-			waypoint.latitude, waypoint.longitude, waypoint.altitude, waypoint.heading, waypoint.curvesize,
-			waypoint.rotationdir, waypoint.gimblemode, waypoint.gimbalpitchangle, waypoint.actiontype1,
-			waypoint.actionparam1, waypoint.actiontype2, waypoint.actionparam2, waypoint.actiontype3,
-			waypoint.actionparam3, waypoint.actiontype4, waypoint.actionparam4, waypoint.actiontype5,
-			waypoint.actionparam5, waypoint.actiontype6, waypoint.actionparam6, waypoint.actiontype7,
-			waypoint.actionparam7, waypoint.actiontype8, waypoint.actionparam8, waypoint.actiontype9,
-			waypoint.actionparam9, waypoint.actiontype10, waypoint.actionparam10, waypoint.actiontype11,
-			waypoint.actionparam11, waypoint.actiontype12, waypoint.actionparam12, waypoint.actiontype13,
-			waypoint.actionparam13, waypoint.actiontype14, waypoint.actionparam14, waypoint.actiontype15,
-			waypoint.actionparam15, waypoint.altitudemode, waypoint.speed, waypoint.poi_latitude,
-			waypoint.poi_longitude, waypoint.poi_altitude, waypoint.poi_altitudemode, waypoint.photo_timeinterval,
-			waypoint.photo_distinterval)
-	}
 }
