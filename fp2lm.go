8f1b1569e9d2b203e7805a34419afbe01e5b4cde
P. D. Finn KD9WEH <pdfinn@users.noreply.github.com>
2023-12-27 14:44:55 +0700

    Added code to adjust aircraft bearing based on waypoints.

--- fp2lm.go
+++ fp2lm.go
@@ -15,89 +15,113 @@ import (
 
 var interval = lenconv.PhotoIntervalFlag("d", 0, "Enter the photo interval (meters 'm' or feet 'ft'). Example: -d 20ft")
 
-func main() {
-	flag.Parse()
+func calculateBearing(lat1, lon1, lat2, lon2 float64) float64 {
+    lat1Rad := lat1 * math.Pi / 180
+    lat2Rad := lat2 * math.Pi / 180
+    lonDiffRad := (lon2 - lon1) * math.Pi / 180
 
-	// create an instance of the wp with default values
-	wp := newWaypoint()
+    y := math.Sin(lonDiffRad) * math.Cos(lat2Rad)
+    x := math.Cos(lat1Rad)*math.Sin(lat2Rad) - math.Sin(lat1Rad)*math.Cos(lat2Rad)*math.Cos(lonDiffRad)
+    bearing := math.Atan2(y, x) * 180 / math.Pi
 
-	// print the Litchi Mission header
-	fmt.Println("latitude, longitude, altitude(m), heading(deg), curvesize(m), rotationdir, gimbalmode, " +
-		"gimbalpitchangle, actiontype1, actionparam1, actiontype2, actionparam2, actiontype3, actionparam3, " +
-		"actiontype4, actionparam4, actiontype5, actionparam5, actiontype6, actionparam6, actiontype7, " +
-		"actionparam7, actiontype8, actionparam8, actiontype9, actionparam9, actiontype10, actionparam10," +
-		" actiontype11, actionparam11, actiontype12, actionparam12, actiontype13, actionparam13, actiontype14," +
-		" actionparam14, actiontype15, actionparam15, altitudemode, speed(m/s), poi_latitude, poi_longitude, " +
-		"poi_altitude(m), poi_altitudemode, photo_timeinterval, photo_distinterval")
-
-	// for each line of standard input, print it as a LitchiMission record
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() != false {
-		// Read the next ln
-		ln := scanner.Text()
-
-		// Create a new CSV reader to parse the ln
-		reader := csv.NewReader(strings.NewReader(ln))
-
-		// Read the rec from the CSV reader
-		rec, err := reader.Read()
-		if err == io.EOF {
-			// End of input, break out of the loop
-			break
-		} else if err != nil {
-			// Other error, print the error and exit
-			fmt.Println("Error reading CSV input:", err)
-			os.Exit(1)
-		}
-
-		// Skip the first rec if it matches the expected header
-		if rec[0] == "Waypoint Number" && rec[1] == "X [m]" && rec[2] == "Y [m]" && rec[3] == "Alt. ASL [m]" && rec[4] == "Alt. AGL [m]" && rec[5] == "xcoord" && rec[6] == "ycoord" {
-			continue
-		}
-
-		// set the specific wp fields; we use a helper function for validation and check
-		// minimum and maximum values
-		longitude, _, err := parseField(rec[5], "float64", -180, 180)
-		if err != nil {
-			fmt.Println("Error parsing longitude:", err)
-			continue
-		}
-		wp.longitude = longitude
-
-		latitude, _, err := parseField(rec[6], "float64", -90, 90)
-		if err != nil {
-			fmt.Println("Error parsing latitude:", err)
-			continue
-		}
-		wp.latitude = latitude
-
-		altitude, _, err := parseField(rec[3], "float64", 0, math.MaxFloat64)
-		if err != nil {
-			fmt.Println("Error parsing altitude:", err)
-			continue
-		}
-		wp.altitude = altitude
-		wp.gimbalpitchangle = -90
-		wp.photo_distinterval = interval
-
-		// print the individual records/waypoints
-		fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, "+
-			"%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v \n",
-			wp.latitude, wp.longitude, wp.altitude, wp.heading, wp.curvesize,
-			wp.rotationdir, wp.gimblemode, wp.gimbalpitchangle, wp.actiontype1,
-			wp.actionparam1, wp.actiontype2, wp.actionparam2, wp.actiontype3,
-			wp.actionparam3, wp.actiontype4, wp.actionparam4, wp.actiontype5,
-			wp.actionparam5, wp.actiontype6, wp.actionparam6, wp.actiontype7,
-			wp.actionparam7, wp.actiontype8, wp.actionparam8, wp.actiontype9,
-			wp.actionparam9, wp.actiontype10, wp.actionparam10, wp.actiontype11,
-			wp.actionparam11, wp.actiontype12, wp.actionparam12, wp.actiontype13,
-			wp.actionparam13, wp.actiontype14, wp.actionparam14, wp.actiontype15,
-			wp.actionparam15, wp.altitudemode, wp.speed, wp.poi_latitude,
-			wp.poi_longitude, wp.poi_altitude, wp.poi_altitudemode, wp.photo_timeinterval,
-			wp.photo_distinterval)
-	}
+    return math.Mod(bearing+360, 360)
 }
+
+func main() {
+    flag.Parse()
+
+    // create an instance of the wp with default values
+    wp := newWaypoint()
+
+    // print the Litchi Mission header
+    fmt.Println("latitude, longitude, altitude(m), heading(deg), curvesize(m), rotationdir, gimbalmode, " +
+        "gimbalpitchangle, actiontype1, actionparam1, actiontype2, actionparam2, actiontype3, actionparam3, " +
+        "actiontype4, actionparam4, actiontype5, actionparam5, actiontype6, actionparam6, actiontype7, " +
+        "actionparam7, actiontype8, actionparam8, actiontype9, actionparam9, actiontype10, actionparam10," +
+        " actiontype11, actionparam11, actiontype12, actionparam12, actiontype13, actionparam13, actiontype14," +
+        " actionparam14, actiontype15, actionparam15, altitudemode, speed(m/s), poi_latitude, poi_longitude, " +
+        "poi_altitude(m), poi_altitudemode, photo_timeinterval, photo_distinterval")
+
+    // for each line of standard input, process it as a LitchiMission record
+    scanner := bufio.NewScanner(os.Stdin)
+    var previousWp *LitchiWaypoint
+    waypoints := []*LitchiWaypoint{}
+
+    for scanner.Scan() {
+        ln := scanner.Text()
+        reader := csv.NewReader(strings.NewReader(ln))
+        rec, err := reader.Read()
+
+        if err == io.EOF {
+            break
+        } else if err != nil {
+            fmt.Println("Error reading CSV input:", err)
+            os.Exit(1)
+        }
+
+        if rec[0] == "Waypoint Number" && rec[1] == "X [m]" && rec[2] == "Y [m]" && rec[3] == "Alt. ASL [m]" && rec[4] == "Alt. AGL [m]" && rec[5] == "xcoord" && rec[6] == "ycoord" {
+            continue
+        }
+
+        wp := newWaypoint() // Create a new waypoint instance for each record
+
+        longitude, _, err := parseField(rec[5], "float64", -180, 180)
+        if err != nil {
+            fmt.Println("Error parsing longitude:", err)
+            continue
+        }
+        wp.longitude = longitude
+
+        latitude, _, err := parseField(rec[6], "float64", -90, 90)
+        if err != nil {
+            fmt.Println("Error parsing latitude:", err)
+            continue
+        }
+        wp.latitude = latitude
+
+        altitude, _, err := parseField(rec[3], "float64", 0, math.MaxFloat64)
+        if err != nil {
+            fmt.Println("Error parsing altitude:", err)
+            continue
+        }
+        wp.altitude = altitude
+        wp.gimbalpitchangle = -90
+        wp.photo_distinterval = interval
+
+        waypoints = append(waypoints, wp)
+
+        if previousWp != nil {
+            previousWp.heading = float32(calculateBearing(previousWp.latitude, previousWp.longitude, wp.latitude, wp.longitude))
+        }
+
+        previousWp = wp
+    }
+
+    if len(waypoints) > 1 {
+        lastWp := waypoints[len(waypoints)-1]
+        secondLastWp := waypoints[len(waypoints)-2]
+        lastWp.heading = secondLastWp.heading
+    }
+
+    // Print the waypoints outside the loop
+    for _, wp := range waypoints {
+        fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, "+
+            "%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v \n",
+            wp.latitude, wp.longitude, wp.altitude, wp.heading, wp.curvesize,
+            wp.rotationdir, wp.gimblemode, wp.gimbalpitchangle, wp.actiontype1,
+            wp.actionparam1, wp.actiontype2, wp.actionparam2, wp.actiontype3,
+            wp.actionparam3, wp.actiontype4, wp.actionparam4, wp.actiontype5,
+            wp.actionparam5, wp.actiontype6, wp.actionparam6, wp.actiontype7,
+            wp.actionparam7, wp.actiontype8, wp.actionparam8, wp.actiontype9,
+            wp.actionparam9, wp.actiontype10, wp.actionparam10, wp.actiontype11,
+            wp.actionparam11, wp.actiontype12, wp.actionparam12, wp.actiontype13,
+            wp.actionparam13, wp.actiontype14, wp.actionparam14, wp.actiontype15,
+            wp.actionparam15, wp.altitudemode, wp.speed, wp.poi_latitude,
+            wp.poi_longitude, wp.poi_altitude, wp.poi_altitudemode, wp.photo_timeinterval,
+            wp.photo_distinterval)
+    }
+}
+
 
 // LitchiWaypoint is a struct to represent the Litchi waypoint
 type LitchiWaypoint struct {
